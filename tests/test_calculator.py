0aeb451b47c002e7129a9af7be87f8a0e3263cc7
Risolat Madaminova <risolatkhon.madaminova@alatoo.edu.kg>
2026-03-13 09:37:56 +0600

    Testing

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -5,7 +5,7 @@ from src.calculator import add, subtract, multiply, divide
 
 
 def test_add():
-    assert add(2, 3) == 5
+    assert add(2, 3) == 6
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
 
